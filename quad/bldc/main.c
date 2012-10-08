c09c0e2fad206dabc00ff6b24b1293b767f4f22a
lxyppc@gmail.com <lxyppc@gmail.com@a1e0bfe9-9f80-e943-2008-5ec03726fcf4>
2012-10-08 10:11:17 +0000

    add dbg usart code

--- quad/bldc/main.c
+++ quad/bldc/main.c
@@ -76,8 +76,9 @@ __IO uint32_t TransmitMode = 0;
 void init_addr_config(void);
 void init_led(void);
 void init_ppm(void);
-void init_usart(void);
 void init_i2c(void);
+void init_usart_dbg(void);
+void init_usart(void);
 void init_adc(void);
 void init_pwm(void);
 void init_tim15(void);
@@ -103,8 +104,9 @@ int main(void)
     init_addr_config();
     init_led();
     init_ppm();
-    init_usart();
     init_i2c();
+    init_usart();
+    init_usart_dbg();
     init_adc();
     init_pwm();
     io_test();
@@ -422,18 +424,18 @@ void init_usart(void)
     
     USART_Cmd(USART2, ENABLE);
 }
-void send_data(const void* p, uint32_t len)
+void send_data(USART_TypeDef* USARTx, const void* p, uint32_t len)
 {
     memcpy(txBuffer,p,len);
     DMA_Cmd(DMA1_Channel4, DISABLE);
-    USART_DMACmd(USART2, USART_DMAReq_Tx, DISABLE);
+    USART_DMACmd(USARTx, USART_DMAReq_Tx, DISABLE);
     
     DMA1_Channel4->CNDTR = len;
     DMA1_Channel4->CPAR = (uint32_t)txBuffer;
     
     DMA_ClearFlag(DMA1_FLAG_GL4);
     
-    USART_DMACmd(USART2, USART_DMAReq_Tx, ENABLE);
+    USART_DMACmd(USARTx, USART_DMAReq_Tx, ENABLE);
     USART_ClearFlag(USART2, USART_FLAG_TC);
     DMA_Cmd(DMA1_Channel4, ENABLE);
 }
@@ -449,6 +451,102 @@ void USART2_IRQHandler(void)
         // when valid data received, we should set the tx pin to AF_1
         // this will disable SWD
         USART_SendData(USART2, data);
+    }
+}
+
+#define USART1_TDR_ADDRESS  ((uint32_t)(USART1_BASE + 0x28))
+// When debug, use usart1 on the i2c port
+// 
+void init_usart_dbg(void)
+{
+    USART_InitTypeDef   USART_InitStructure;
+    GPIO_InitTypeDef    GPIO_InitStructure;
+    NVIC_InitTypeDef    NVIC_InitStructure;
+    DMA_InitTypeDef         DMA_InitStructure;
+    
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1, ENABLE);
+    
+    USART_InitStructure.USART_BaudRate = 115200;
+    USART_InitStructure.USART_WordLength = USART_WordLength_8b;
+    USART_InitStructure.USART_StopBits = USART_StopBits_1;
+    USART_InitStructure.USART_Parity = USART_Parity_No;
+    USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
+    USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
+    USART_Init(USART1, &USART_InitStructure);
+    
+    /* Configure USART Tx and Rx as alternate function push-pull */
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_Level_3;
+    GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+    GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP;
+
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_6;
+    GPIO_Init(GPIOB, &GPIO_InitStructure);
+    
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_7;
+    GPIO_Init(GPIOB, &GPIO_InitStructure);
+    
+    /* Connect PB6 to USART1_Tx */
+    GPIO_PinAFConfig(GPIOB, GPIO_PinSource6, GPIO_AF_0);
+    
+    /* Connect PB7 to USART1_Rx */
+    GPIO_PinAFConfig(GPIOB, GPIO_PinSource7, GPIO_AF_0);
+    
+    /* Enable the UASRT1 global Interrupt */
+    NVIC_InitStructure.NVIC_IRQChannel = USART1_IRQn;
+    NVIC_InitStructure.NVIC_IRQChannelPriority = 1;
+    NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+    NVIC_Init(&NVIC_InitStructure);
+    
+    // Remap the USART tx1 dma to channel 4
+    SYSCFG_DMAChannelRemapConfig(SYSCFG_DMARemap_USART1Tx, ENABLE);
+    
+    // Use DMA to send data
+    //USART_DMACmd(USART2, USART_DMAReq_Tx, ENABLE);
+    DMA_DeInit(DMA1_Channel4);
+    
+    DMA_InitStructure.DMA_PeripheralBaseAddr = (uint32_t)USART1_TDR_ADDRESS;
+    DMA_InitStructure.DMA_MemoryBaseAddr = (uint32_t)txBuffer;
+    DMA_InitStructure.DMA_DIR = DMA_DIR_PeripheralDST;
+    DMA_InitStructure.DMA_BufferSize = 64;
+    DMA_InitStructure.DMA_PeripheralInc = DMA_PeripheralInc_Disable;
+    DMA_InitStructure.DMA_MemoryInc = DMA_MemoryInc_Enable;
+    DMA_InitStructure.DMA_PeripheralDataSize = DMA_PeripheralDataSize_Byte;
+    DMA_InitStructure.DMA_MemoryDataSize = DMA_PeripheralDataSize_Byte;
+    DMA_InitStructure.DMA_Mode = DMA_Mode_Normal;
+    DMA_InitStructure.DMA_Priority = DMA_Priority_Medium;
+    
+    DMA_InitStructure.DMA_M2M = DMA_M2M_Disable;
+    DMA_Init(DMA1_Channel4, &DMA_InitStructure);
+    
+    //DMA_Cmd(DMA1_Channel4, ENABLE);
+    
+    NVIC_InitStructure.NVIC_IRQChannel = DMA1_Channel4_5_IRQn;
+    NVIC_InitStructure.NVIC_IRQChannelPriority = 1;
+    NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+    NVIC_Init(&NVIC_InitStructure);
+    
+    
+    DMA_ITConfig(DMA1_Channel4, DMA_IT_TC | DMA_IT_TE, ENABLE);
+    
+    
+    USART_ITConfig(USART1, USART_IT_RXNE, ENABLE);
+    
+    USART_Cmd(USART1, ENABLE);
+}
+
+// USART2 IRQ handler
+void USART1_IRQHandler(void)
+{
+    /* USART in mode Receiver --------------------------------------------------*/
+    if (USART_GetITStatus(USART1, USART_IT_RXNE) == SET)
+    {
+        uint16_t data = (uint8_t)USART_ReceiveData(USART1);
+        // use systick for time out
+        process_data((uint8_t)data);
+        // when valid data received, we should set the tx pin to AF_1
+        // this will disable SWD
+        USART_SendData(USART1, data);
     }
 }
 
