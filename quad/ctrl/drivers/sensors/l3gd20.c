b0ac2950169c9fa5dcd41ee16f23ec4aec46de40
lxyppc@gmail.com <lxyppc@gmail.com@a1e0bfe9-9f80-e943-2008-5ec03726fcf4>
2013-01-10 09:58:11 +0000

    update sensor driver

--- quad/ctrl/drivers/sensors/l3gd20.c
+++ quad/ctrl/drivers/sensors/l3gd20.c
@@ -77,6 +77,11 @@ void L3GD20_gyro_init(void)
 
     temp |= L3GD20_HIGHPASSFILTER_ENABLE;
     L3GD20_Write_buffer(&temp, L3GD20_CTRL_REG5_ADDR, 1);
+    
+    // enable data ready interrupt
+    L3GD20_Read_buffer(&temp, L3GD20_CTRL_REG3_ADDR, 1);
+    temp |= L3GD20_INT2INTERRUPT_ENABLE;
+    L3GD20_Write_buffer(&temp, L3GD20_CTRL_REG3_ADDR, 1);
 }
 
 void L3GD20_gyro_read(void)
@@ -86,8 +91,44 @@ void L3GD20_gyro_read(void)
     L3GD20_Read_buffer(buffer,L3GD20_OUT_X_L_ADDR,6);
 }
 
+void read_raw_gyro(int16_t * data)
+{
+    L3GD20_Read_buffer((uint8_t*)data,L3GD20_OUT_X_L_ADDR,6);
+    // setting is LSB, no need to swap bytes order
+}
 
 
+#define L3G_250dps     (float)114.285f         /*!< gyroscope sensitivity with 250 dps full scale [LSB/dps] */
+#define L3G_500dps     (float)57.1429f        /*!< gyroscope sensitivity with 500 dps full scale [LSB/dps] */
+#define L3G_2000dps    (float)14.285f	      /*!< gyroscope sensitivity with 2000 dps full scale [LSB/dps] */
+#define _PI            (float)3.14159265f
 
+void read_gyro_normalized(int32_t* data)
+{
+    int16_t buffer[3];
+    read_raw_gyro(buffer);
+    data[0] = buffer[0]*500 / 28571;
+    data[1] = buffer[1]*500 / 28571;
+    data[2] = buffer[2]*500 / 28571;
+}
+
+void read_gyro_normalized_f(int32_t* data)
+{
+    int16_t buffer[3];
+    read_raw_gyro(buffer);
+    data[0] = buffer[0]/L3G_500dps;
+    data[1] = buffer[1]/L3G_500dps;
+    data[2] = buffer[2]/L3G_500dps;
+}
+
+void l3gd20_int2_irq_handler(void)
+{
+    uint8_t temp;
+    L3GD20_Read_buffer(&temp, L3GD20_STATUS_REG_ADDR, 1);
+    if(temp & 0x0f){
+        int32_t gyro[3];
+        read_gyro_normalized(gyro);
+    }
+}
 
 
--- quad/ctrl/drivers/sensors/lsm303dlhc.c
+++ quad/ctrl/drivers/sensors/lsm303dlhc.c
@@ -1,6 +1,7 @@
 #include "bsp.h"
 #include "lsm303dlhc.h"
 
+#define  LSM303DLHC_MAG_GAIN    LSM303DLHC_FS_8_1_GA
 
 static uint8_t inited = 0;
 void LSM303DLHC_acc_init(void)
@@ -55,6 +56,40 @@ void LSM303DLHC_acc_read(void)
     // we need normalize the value
 }
 
+
+void read_raw_acc(int16_t * data)
+{
+    int16_t buffer[3];
+    LSM303DLHC_Read_Buffer(ACC_I2C_ADDRESS, LSM303DLHC_OUT_X_L_A, (uint8_t*)buffer, 6);
+    // we config it as LSB first, no need to swap bytes order
+}
+
+
+// normalize to micro G
+// data[0]  - x acc micro G
+// data[1]  - y acc micro G
+// data[2]  - z acc micro G
+void read_acc_normalized(int32_t* data)
+{
+    int16_t buffer[3];
+    read_raw_acc(buffer);
+    // for 2G without fifo
+    data[0] = buffer[0]*1000/16;
+    data[1] = buffer[1]*1000/16;
+    data[2] = buffer[2]*1000/16;
+}
+
+void read_acc_normalized_f(float* data)
+{
+    int16_t buffer[3];
+    read_raw_acc(buffer);
+    // for 2G without fifo
+    data[0] = buffer[0]*1000.0/16.0;
+    data[1] = buffer[1]*1000.0/16.0;
+    data[2] = buffer[2]*1000.0/16.0;
+}
+
+
 #define swap_(x,y)  do{uint8_t t =x; x = y; y = t;}while(0)
 void LSM303DLHC_mag_read(uint8_t* buffer)
 {
@@ -63,12 +98,106 @@ void LSM303DLHC_mag_read(uint8_t* buffer)
     //swap_(buffer[0], buffer[1]);
     swap_(buffer[2], buffer[4]);
     swap_(buffer[3], buffer[5]);
-    
     // we need normalize the value
 }
 
+// data[0] - x mag, int16
+// data[1] - y mag, int16
+// data[2] - z mag, int16
+void read_raw_mag(int16_t * data)
+{
+    uint8_t * buffer = (uint8_t*) data;
+    LSM303DLHC_Read_Buffer(MAG_I2C_ADDRESS, LSM303DLHC_OUT_X_H_M, buffer, 6);
+    swap_(buffer[0], buffer[1]); // swap the byte order
+    
+    // this need unaligned access support
+    //*(uint32_t*)(data + 1) = __REV( *(uint32_t*)(data + 1));
+    swap_(buffer[2], buffer[5]); // swap the byte order and y,z
+    swap_(buffer[3], buffer[4]);
+}
 
+#ifndef L3GD20_CS_HIGH
+void L3GD20_CS_HIGH(void){}
+void L3GD20_CS_LOW(void){}
+void L3GD20_INIT(void){}
+#endif
+    
+// data[0]  - x mag micro Gauss
+// data[1]  - y mag micro Gauss
+// data[2]  - z mag micro Gauss
+void read_mag_normalized(int32_t * data)
+{
+    int16_t buffer[3];
+    int32_t LSM303DLHC_M_SENSITIVITY_XY;
+    int32_t LSM303DLHC_M_SENSITIVITY_Z;
+    if (LSM303DLHC_MAG_GAIN == LSM303DLHC_FS_1_3_GA){
+      LSM303DLHC_M_SENSITIVITY_XY = LSM303DLHC_M_SENSITIVITY_XY_1_3Ga;
+      LSM303DLHC_M_SENSITIVITY_Z = LSM303DLHC_M_SENSITIVITY_Z_1_3Ga;
+    }else if( LSM303DLHC_MAG_GAIN == LSM303DLHC_FS_1_9_GA){
+      LSM303DLHC_M_SENSITIVITY_XY  =  LSM303DLHC_M_SENSITIVITY_XY_1_9Ga;
+      LSM303DLHC_M_SENSITIVITY_Z   = LSM303DLHC_M_SENSITIVITY_Z_1_9Ga;
+    }else if( LSM303DLHC_MAG_GAIN == LSM303DLHC_FS_2_5_GA){
+      LSM303DLHC_M_SENSITIVITY_XY  =  LSM303DLHC_M_SENSITIVITY_XY_2_5Ga;
+      LSM303DLHC_M_SENSITIVITY_Z  =  LSM303DLHC_M_SENSITIVITY_Z_2_5Ga;
+    }else if( LSM303DLHC_MAG_GAIN == LSM303DLHC_FS_4_0_GA){
+      LSM303DLHC_M_SENSITIVITY_XY  =  LSM303DLHC_M_SENSITIVITY_XY_4Ga;
+      LSM303DLHC_M_SENSITIVITY_Z  =  LSM303DLHC_M_SENSITIVITY_Z_4Ga;
+    }else if( LSM303DLHC_MAG_GAIN == LSM303DLHC_FS_4_7_GA){
+      LSM303DLHC_M_SENSITIVITY_XY  =  LSM303DLHC_M_SENSITIVITY_XY_4_7Ga;
+      LSM303DLHC_M_SENSITIVITY_Z  =  LSM303DLHC_M_SENSITIVITY_Z_4_7Ga;
+    }else if( LSM303DLHC_MAG_GAIN == LSM303DLHC_FS_5_6_GA){
+      LSM303DLHC_M_SENSITIVITY_XY  =  LSM303DLHC_M_SENSITIVITY_XY_5_6Ga;
+      LSM303DLHC_M_SENSITIVITY_Z  =  LSM303DLHC_M_SENSITIVITY_Z_5_6Ga;
+    }else if( LSM303DLHC_MAG_GAIN == LSM303DLHC_FS_8_1_GA){
+      LSM303DLHC_M_SENSITIVITY_XY  =  LSM303DLHC_M_SENSITIVITY_XY_8_1Ga;
+      LSM303DLHC_M_SENSITIVITY_Z  =  LSM303DLHC_M_SENSITIVITY_Z_8_1Ga;
+    }else{ 
+      LSM303DLHC_M_SENSITIVITY_XY  =  LSM303DLHC_M_SENSITIVITY_XY_1_3Ga;
+      LSM303DLHC_M_SENSITIVITY_Z  =  LSM303DLHC_M_SENSITIVITY_Z_1_3Ga;
+    }
+    read_raw_mag(buffer);
+    data[0] = buffer[0]*1000/LSM303DLHC_M_SENSITIVITY_XY;
+    data[1] = buffer[1]*1000/LSM303DLHC_M_SENSITIVITY_XY;
+    data[2] = buffer[2]*1000/LSM303DLHC_M_SENSITIVITY_Z;
+}
 
-
+// data[0]  - x mag micro Gauss
+// data[1]  - y mag micro Gauss
+// data[2]  - z mag micro Gauss
+void read_mag_normalized_f(float * data)
+{
+    int16_t buffer[3];
+    int32_t LSM303DLHC_M_SENSITIVITY_XY;
+    int32_t LSM303DLHC_M_SENSITIVITY_Z;
+    if (LSM303DLHC_MAG_GAIN == LSM303DLHC_FS_1_3_GA){
+      LSM303DLHC_M_SENSITIVITY_XY = LSM303DLHC_M_SENSITIVITY_XY_1_3Ga;
+      LSM303DLHC_M_SENSITIVITY_Z = LSM303DLHC_M_SENSITIVITY_Z_1_3Ga;
+    }else if( LSM303DLHC_MAG_GAIN == LSM303DLHC_FS_1_9_GA){
+      LSM303DLHC_M_SENSITIVITY_XY  =  LSM303DLHC_M_SENSITIVITY_XY_1_9Ga;
+      LSM303DLHC_M_SENSITIVITY_Z   = LSM303DLHC_M_SENSITIVITY_Z_1_9Ga;
+    }else if( LSM303DLHC_MAG_GAIN == LSM303DLHC_FS_2_5_GA){
+      LSM303DLHC_M_SENSITIVITY_XY  =  LSM303DLHC_M_SENSITIVITY_XY_2_5Ga;
+      LSM303DLHC_M_SENSITIVITY_Z  =  LSM303DLHC_M_SENSITIVITY_Z_2_5Ga;
+    }else if( LSM303DLHC_MAG_GAIN == LSM303DLHC_FS_4_0_GA){
+      LSM303DLHC_M_SENSITIVITY_XY  =  LSM303DLHC_M_SENSITIVITY_XY_4Ga;
+      LSM303DLHC_M_SENSITIVITY_Z  =  LSM303DLHC_M_SENSITIVITY_Z_4Ga;
+    }else if( LSM303DLHC_MAG_GAIN == LSM303DLHC_FS_4_7_GA){
+      LSM303DLHC_M_SENSITIVITY_XY  =  LSM303DLHC_M_SENSITIVITY_XY_4_7Ga;
+      LSM303DLHC_M_SENSITIVITY_Z  =  LSM303DLHC_M_SENSITIVITY_Z_4_7Ga;
+    }else if( LSM303DLHC_MAG_GAIN == LSM303DLHC_FS_5_6_GA){
+      LSM303DLHC_M_SENSITIVITY_XY  =  LSM303DLHC_M_SENSITIVITY_XY_5_6Ga;
+      LSM303DLHC_M_SENSITIVITY_Z  =  LSM303DLHC_M_SENSITIVITY_Z_5_6Ga;
+    }else if( LSM303DLHC_MAG_GAIN == LSM303DLHC_FS_8_1_GA){
+      LSM303DLHC_M_SENSITIVITY_XY  =  LSM303DLHC_M_SENSITIVITY_XY_8_1Ga;
+      LSM303DLHC_M_SENSITIVITY_Z  =  LSM303DLHC_M_SENSITIVITY_Z_8_1Ga;
+    }else{ 
+      LSM303DLHC_M_SENSITIVITY_XY  =  LSM303DLHC_M_SENSITIVITY_XY_1_3Ga;
+      LSM303DLHC_M_SENSITIVITY_Z  =  LSM303DLHC_M_SENSITIVITY_Z_1_3Ga;
+    }
+    read_raw_mag(buffer);
+    data[0] = buffer[0]*1000.0/LSM303DLHC_M_SENSITIVITY_XY;
+    data[1] = buffer[1]*1000.0/LSM303DLHC_M_SENSITIVITY_XY;
+    data[2] = buffer[2]*1000.0/LSM303DLHC_M_SENSITIVITY_Z;
+}
 
 
