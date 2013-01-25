dbbf427a95ff071fe94d865fdbe8ac3af7460d16
lxyppc@gmail.com <lxyppc@gmail.com@a1e0bfe9-9f80-e943-2008-5ec03726fcf4>
2013-01-25 14:00:41 +0000

    add dynamic UI test files

---
+++ quad/ctrl/script/testui.lua
@@ -0,0 +1,31 @@
+ld = QUiLoader()
+w = ld:load("test.ui")
+dlg = QDialog()
+
+
+function parse_objects(w)
+    for k,btn in pairs(QPushButton.findChildren(w)) do
+       w[btn.objectName] = btn
+    end
+end
+parse_objects(w)
+
+ls = QListWidget.findChild(w)
+function btn_clicked(btn)
+  ls:addItem(btn.text .. " -> clicked")
+end
+
+--for k,btn in pairs(QPushButton.findChildren(w)) do
+--   btn.clicked = {btn,btn_clicked}
+--end
+
+w["btn1"].clicked = {w["btn1"],btn_clicked}
+w["btn2"].clicked = {w["btn2"],btn_clicked}
+w["btn3"].clicked = {w["btn3"],btn_clicked}
+
+
+dlg.layout = QVBoxLayout{
+   w,
+}
+
+dlg:exec()
